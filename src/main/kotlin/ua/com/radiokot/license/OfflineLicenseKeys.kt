dcac7af9c0a3178b2e867756d56464a37f29a2b2
Oleg Koretsky <oleg@radiokot.com.ua>
2024-03-25 21:14:10 +0200

    Prettify OfflineLicenseKeys in Java

--- src/main/kotlin/ua/com/radiokot/license/OfflineLicenseKeys.kt
+++ src/main/kotlin/ua/com/radiokot/license/OfflineLicenseKeys.kt
@@ -1,10 +1,16 @@
 package ua.com.radiokot.license
 
+/**
+ * Provides suites to work with different
+ * offline license key formats.
+ */
 object OfflineLicenseKeys {
     /**
      * A suite to work with keys in JWT (JSON Web Token) format.
      *
      * @see JwtLicenseKey
      */
+    @JvmStatic
+    @get:JvmName("jwt")
     val jwt = JwtLicenseKeySuite()
 }
